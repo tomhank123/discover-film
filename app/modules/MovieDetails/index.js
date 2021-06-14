7efb9fe2faf3d0d769498906fe4e143b349058e1
Duc Tran Minh <ductm@devblock.net>
2021-06-14 22:39:09 +0700

    feat: integrate movie details

--- app/modules/MovieDetails/index.js
+++ app/modules/MovieDetails/index.js
@@ -4,25 +4,38 @@
  *
  */
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Helmet } from 'react-helmet';
-import { FormattedMessage } from 'react-intl';
 import { createStructuredSelector } from 'reselect';
-import { compose } from 'redux';
+import { bindActionCreators, compose } from 'redux';
 
 import { useInjectSaga } from 'utils/injectSaga';
 import { useInjectReducer } from 'utils/injectReducer';
+import * as movieUtils from 'utils/movieUtils';
+import { Container } from 'react-bootstrap';
 import Header from 'components/Header';
-import makeSelectMovieDetails from './selectors';
+import MovieArticle from './MovieArticle';
+import * as actions from './actions';
+import { makeSelectDetails } from './selectors';
 import reducer from './reducer';
 import saga from './saga';
-import messages from './messages';
 
-export function MovieDetails() {
+export function MovieDetails({ details, onLoadDetails, ...restProps }) {
   useInjectReducer({ key: 'movieDetails', reducer });
   useInjectSaga({ key: 'movieDetails', saga });
+
+  const { location } = restProps;
+  const movieId = movieUtils.getIdFromRoute(location);
+
+  useEffect(() => {
+    if (movieId) {
+      onLoadDetails({ movieId });
+    }
+  }, [movieId]);
+
+  if (!movieId) return null;
 
   return (
     <div>
@@ -31,22 +44,27 @@ export function MovieDetails() {
         <meta name="description" content="Description of MovieDetails" />
       </Helmet>
       <Header />
-      <FormattedMessage {...messages.header} />
+      <Container>
+        <MovieArticle {...details} />
+      </Container>
     </div>
   );
 }
 
 MovieDetails.propTypes = {
-  dispatch: PropTypes.func.isRequired,
+  details: PropTypes.object,
+  onLoadDetails: PropTypes.func,
 };
 
 const mapStateToProps = createStructuredSelector({
-  movieDetails: makeSelectMovieDetails(),
+  details: makeSelectDetails(),
 });
 
 function mapDispatchToProps(dispatch) {
+  const onLoadDetails = actions.getDetails.request;
+
   return {
-    dispatch,
+    ...bindActionCreators({ onLoadDetails }, dispatch),
   };
 }
 
--- app/utils/movieUtils.js
+++ app/utils/movieUtils.js
@@ -1,5 +1,17 @@
+import { matchPath } from 'react-router';
 import truncate from 'lodash/truncate';
 import * as ROUTES from 'routes/constants';
+
+export const getIdFromRoute = location => {
+  const match = matchPath(location.pathname, {
+    path: '/movie/:movieId',
+    exact: true,
+    strict: true,
+  });
+  const movieId = +match.params.movieId;
+
+  return movieId;
+};
 
 export const getPoster = poster =>
   poster ? `https://www.themoviedb.org/t/p/original/${poster}` : poster;
