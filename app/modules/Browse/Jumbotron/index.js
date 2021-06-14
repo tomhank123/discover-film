2bef99963a9f8b814c1678d44be38ddd8c02e2c0
Duc Tran Minh <ductm@devblock.net>
2021-06-14 17:13:53 +0700

    refactor: update jumbotron

--- app/modules/Browse/Jumbotron/index.js
+++ app/modules/Browse/Jumbotron/index.js
@@ -13,10 +13,11 @@ function Jumbotron() {
     <Card bg="secondary" className="rounded-0" text="light">
       <Container>
         <Card.Body className="px-0 py-5">
-          <h1 className="display-4 font-weight-bold">Welcome.</h1>
-          <p className="lead">
-            Millions of movies, TV shows and people to discover. Explore now.
-          </p>
+          <Card.Title className="display-4 fw-bold">Welcome.</Card.Title>
+          <Card.Text className="lead">
+            Millions of movies, TV shows and Card.Texteople to discover. Explore
+            now.
+          </Card.Text>
         </Card.Body>
       </Container>
     </Card>
