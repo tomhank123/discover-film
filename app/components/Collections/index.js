8e80bb80d225902aed05e2b4b74c07f485b1b063
Duc Tran Minh <ductm@devblock.net>
2021-06-14 18:38:01 +0700

    fix: fix collection error

--- app/components/Collections/index.js
+++ app/components/Collections/index.js
@@ -9,7 +9,7 @@ import PropTypes from 'prop-types';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import Skeleton from 'react-loading-skeleton';
 
-import { Alert, Card, Button } from 'react-bootstrap';
+import { Alert } from 'react-bootstrap';
 import Titles from 'components/Titles';
 import swiperOptions from './config';
 import MovieList from './MovieList';
@@ -17,27 +17,6 @@ import Wrapper from './Wrapper';
 
 function Collections({ loading, error, items, isSwiper = false }) {
   if (loading) {
-    const CardLoader = () => (
-      <Card className="border-0 shadow-sm">
-        <Skeleton
-          wrapper="div"
-          style={{
-            paddingTop: '150%',
-          }}
-        />
-        <Card.Body>
-          <div className="d-grid gap-2">
-            <Button variant="secondary" size="sm">
-              Watch Now
-            </Button>
-            <Button variant="outline-secondary" size="sm">
-              Watch List
-            </Button>
-          </div>
-        </Card.Body>
-      </Card>
-    );
-
     return (
       <Wrapper>
         {[1, 2, 3, 4, 5, 6].map(id => (
@@ -47,14 +26,25 @@ function Collections({ loading, error, items, isSwiper = false }) {
               <Swiper {...swiperOptions}>
                 {[7, 8, 9, 10, 11, 12, 13, 14, 15].map(childId => (
                   <SwiperSlide key={childId}>
-                    <CardLoader />
+                    <Skeleton
+                      wrapper="div"
+                      style={{
+                        paddingTop: '150%',
+                      }}
+                    />
                   </SwiperSlide>
                 ))}
               </Swiper>
             ) : (
               <MovieList>
                 {[7, 8, 9, 10, 11, 12, 13, 14, 15].map(childId => (
-                  <CardLoader key={childId} />
+                  <Skeleton
+                    key={childId}
+                    wrapper="div"
+                    style={{
+                      paddingTop: '150%',
+                    }}
+                  />
                 ))}
               </MovieList>
             )}
