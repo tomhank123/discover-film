a464c0265eed9a7dcb9c8d0fd1172899083393ee
Duc Tran Minh <ductm@devblock.net>
2021-06-16 15:43:24 +0700

    refactor: update titles, person card

--- app/components/PersonItem/index.js
+++ app/components/PersonItem/index.js
@@ -4,18 +4,21 @@
  *
  */
 
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import Skeleton from 'react-loading-skeleton';
 import { Card, Button, Figure } from 'react-bootstrap';
 import * as personUtils from 'utils/personUtils';
+import { ThemeContext } from 'context/theme-context';
 import StyledPoster from './StyledPoster';
 
 function PersonItem({ loading, item }) {
+  const { darkMode } = useContext(ThemeContext);
+
   if (loading) {
     return (
-      <Card className="border-0 shadow">
+      <Card className="border-0 shadow" bg={darkMode ? 'secondary' : 'light'}>
         <Card.Body className="text-center">
           <Figure className="w-75">
             <Skeleton width={120} height={120} circle />
@@ -23,7 +26,10 @@ function PersonItem({ loading, item }) {
           <Skeleton wrapper="h5" />
           <Skeleton wrapper="p" width="75%" />
           <div className="d-grid gap-2">
-            <Button variant="outline-secondary" size="sm">
+            <Button
+              variant={darkMode ? 'outline-light' : 'outline-secondary'}
+              size="sm"
+            >
               More Info
             </Button>
           </div>
@@ -37,25 +43,36 @@ function PersonItem({ loading, item }) {
     const url = personUtils.getUrl(item.id);
 
     return (
-      <Card className="border-0 shadow">
+      <Card className="border-0 shadow" bg={darkMode ? 'secondary' : 'light'}>
         <Card.Body className="text-center">
           <Figure className="w-75 img-thumbnail rounded-circle">
             <StyledPoster poster={poster} />
           </Figure>
           <Card.Title
-            className="text-truncate text-success mb-0"
+            className={`text-truncate mb-0 ${
+              darkMode ? 'text-warning' : 'text-success'
+            }`}
             title={item.name}
           >
             {item.name}
           </Card.Title>
-          <Card.Text className="text-muted font-monospace">
+          <Card.Text
+            className={`font-monospace ${
+              darkMode ? 'text-light' : 'text-muted'
+            }`}
+          >
             {item.known_for_department}
           </Card.Text>
           <Card.Text className="small m-0" hidden>
             {item.popularity}
           </Card.Text>
           <div className="d-grid gap-2">
-            <Button variant="outline-secondary" size="sm" as={Link} to={url}>
+            <Button
+              variant={darkMode ? 'outline-light' : 'outline-secondary'}
+              size="sm"
+              as={Link}
+              to={url}
+            >
               More Info
             </Button>
           </div>
--- app/components/Titles/index.js
+++ app/components/Titles/index.js
@@ -4,19 +4,25 @@
  *
  */
 
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import Skeleton from 'react-loading-skeleton';
 import { Card, Button } from 'react-bootstrap';
 import * as movieUtils from 'utils/movieUtils';
 import * as tvUtils from 'utils/tvUtils';
+import { ThemeContext } from 'context/theme-context';
 import StyledPoster from './StyledPoster';
 
 function Titles({ loading, item }) {
+  const { darkMode } = useContext(ThemeContext);
+
   if (loading) {
     return (
-      <Card className="border-0 shadow-sm h-100">
+      <Card
+        className="border-0 shadow-sm h-100"
+        bg={darkMode ? 'secondary' : 'light'}
+      >
         <Skeleton
           wrapper="div"
           style={{
@@ -25,10 +31,13 @@ function Titles({ loading, item }) {
         />
         <Card.Body>
           <div className="d-grid gap-2">
-            <Button variant="secondary" size="sm">
+            <Button variant={darkMode ? 'dark' : 'secondary'} size="sm">
               Watch Now
             </Button>
-            <Button variant="outline-secondary" size="sm">
+            <Button
+              variant={darkMode ? 'outline-dark' : 'outline-secondary'}
+              size="sm"
+            >
               Watch List
             </Button>
           </div>
@@ -44,14 +53,27 @@ function Titles({ loading, item }) {
     const title = item.title || item.name;
 
     return (
-      <Card className="border-0 shadow-sm h-100">
+      <Card
+        className="border-0 shadow-sm h-100"
+        bg={darkMode ? 'secondary' : 'light'}
+      >
         <StyledPoster poster={poster} fallback={title} />
         <Card.Body>
           <div className="d-grid gap-2">
-            <Button variant="secondary" size="sm" as={Link} to={url}>
+            <Button
+              variant={darkMode ? 'dark' : 'secondary'}
+              size="sm"
+              as={Link}
+              to={url}
+            >
               Watch Now
             </Button>
-            <Button variant="outline-secondary" size="sm" as={Link} to={url}>
+            <Button
+              variant={darkMode ? 'outline-dark' : 'outline-secondary'}
+              size="sm"
+              as={Link}
+              to={url}
+            >
               Watch List
             </Button>
           </div>
